74bde15dbe1a2f250e44eb522f845f3eee0670e1
Cherrie Awang <caflores31@gmail.com>
2021-05-14 13:02:03 -0700

    update file streuctures

--- server.js
+++ server.js
@@ -7,16 +7,16 @@ const helpers = require('./utils/helpers');
 
 const exphbs = require('express-handlebars');
 const hbs = exphbs.create({ helpers });
-
+//Session
 const session = require('express-session');
-
+//Initialize the server
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
-
+//Set-up the actual session
 const sess = {
-    secret: 'Super secret secret',
+    secret: 'secret',
     cookie: { maxAge: 36000 },
     resave: false,
     saveUninitialized: true,
@@ -25,11 +25,11 @@ const sess = {
     })
 };
 app.use(session(sess));
-
+//middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
-
+//Set handlebars as render engine
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
 
