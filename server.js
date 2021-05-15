87904aed226f8935e01ecf206d722e1621304400
Cherrie Awang <caflores31@gmail.com>
2021-05-14 23:39:06 -0700

    changes made to the project

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ const hbs = exphbs.create({ helpers });
 const session = require('express-session');
 //Initialize the server
 const app = express();
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 5000;
 
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 //Set-up the actual session
